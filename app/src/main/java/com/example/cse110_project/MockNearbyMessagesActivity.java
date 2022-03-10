fbf20fb67b275d8703648d5edf86c2cfca7ff7f7
apple <apple@apples-MacBook-Pro-4.local>
2022-03-09 21:41:11 -0800

    ignore

--- app/src/main/java/com/example/cse110_project/MockNearbyMessagesActivity.java
+++ app/src/main/java/com/example/cse110_project/MockNearbyMessagesActivity.java
@@ -42,6 +42,7 @@ public class MockNearbyMessagesActivity extends AppCompatActivity {
      * If mocked student is waving to User:
      * [0]SUUID,[1]Name,[2]Link,[3]Year,[4]Quarter,[5]Course,[6]CourseNumber,[7]ClassSize,
      * [8]OUUID,[9]Wave
+     *
      * If mocked student is not waving to User:
      * [0]SUUID,[1]Name,[2]Link,[3]Year,[4]Quarter,[5]Course,[6]CourseNumber,[7]ClassSize
      * */
