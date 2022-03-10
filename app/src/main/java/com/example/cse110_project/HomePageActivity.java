37854c499a60776a59edfdf1a3c37812d41ec669
apple <apple@apples-MacBook-Pro-4.local>
2022-03-09 18:47:38 -0800

    refactored home page

--- app/src/main/java/com/example/cse110_project/HomePageActivity.java
+++ app/src/main/java/com/example/cse110_project/HomePageActivity.java
@@ -11,7 +11,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,6 +24,7 @@ import android.widget.TextView;
 import com.example.cse110_project.databases.AppDatabase;
 import com.example.cse110_project.databases.bof.BoFCourse;
 import com.example.cse110_project.databases.bof.BoFStudent;
+import com.example.cse110_project.databases.bof.BoFStudentDao;
 import com.example.cse110_project.databases.def.DefaultCourse;
 import com.example.cse110_project.databases.def.DefaultStudent;
 import com.example.cse110_project.databases.user.UserCourse;
@@ -34,110 +37,152 @@ import com.example.cse110_project.utilities.comparators.PrioritizeSmallClassesCo
 
 import java.util.List;
 
-
-
 public class HomePageActivity extends AppCompatActivity {
     private AppDatabase db;
-    private boolean Start;
-    private Spinner sortingOptionsDropdown;
+    private boolean searchButtonState;
     protected RecyclerView studentsRecyclerView;
     protected RecyclerView.LayoutManager studentsLayoutManager;
     protected BoFStudentViewAdapter studentsViewAdapter;
 
+    /**
+     * Inner class for specifying what happens when a particular sorting option is selected at any
+     * point in time on the home page
+     * */
     class SortingOptions implements AdapterView.OnItemSelectedListener {
 
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-            String sortingOption = adapterView.getItemAtPosition(i).toString();
+            Log.d("HomePageActivity.SortingOptions::onItemSelected()", "Non-testable method");
 
             if (db == null) { return; }
 
+            String sortingOption = adapterView.getItemAtPosition(i).toString();
+
             if (sortingOption.equals("Default")) {
-                displayBirdsOfAFeatherList(new DefaultBoFComparator(db.BoFCourseDao()));
+                displayBirdsOfAFeatherList(db.BoFStudentDao(), new DefaultBoFComparator(db.BoFCourseDao()));
             } else if (sortingOption.equals("Prioritize Recent")) {
-                displayBirdsOfAFeatherList(new PrioritizeMostRecentComparator());
+                displayBirdsOfAFeatherList(db.BoFStudentDao(), new PrioritizeMostRecentComparator());
             } else {
-                displayBirdsOfAFeatherList(new PrioritizeSmallClassesComparator());
+                displayBirdsOfAFeatherList(db.BoFStudentDao(), new PrioritizeSmallClassesComparator());
             }
         }
 
         @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {}
+        public void onNothingSelected(AdapterView<?> adapterView) {
+            Log.d("HomePageActivity.SortingOptions::onNothingSelected()", "Non-testable method");
+        }
 
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("HomePageActivity::onCreate()", "Non-testable method");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         setTitle(Constants.APP_VERSION);
 
-        initSortingOptions();
+        initSortingOptionsDropdown();
+        checkSelectedSortingOption();
+        checkStateOfSearchButton();
+    }
+
+    public void saveSortingOption() {
+        Log.d("HomePageActivity::saveSortingOption()", "Non-testable method");
+
+        SharedPreferences sortingOptionSP = getSharedPreferences("SortingOption", MODE_PRIVATE);
+        SharedPreferences.Editor sortingEditor = sortingOptionSP.edit();
+        Spinner sortingOptionDropdown = findViewById(R.id.sorting_options_dropdown_menu);
+
+        sortingEditor.putString("sortingOption", sortingOptionDropdown.getSelectedItem().toString());
+        sortingEditor.apply();
+    }
+
+    public void saveSearchButtonState() {
+        Log.d("HomePageActivity::saveSearchButtonState()", "Non-testable method");
+
+        SharedPreferences searchButtonSP = getSharedPreferences("SearchButton", MODE_PRIVATE);
+        SharedPreferences.Editor searchEditor = searchButtonSP.edit();
+
+        searchEditor.putBoolean("searchButtonState", this.searchButtonState);
+        searchEditor.apply();
+    }
+
+    public void checkSelectedSortingOption() {
+        Log.d("HomePageActivity::checkSelectedSortingOption()", "Non-testable method");
+
+        Spinner sortingOptionDropdown = findViewById(R.id.sorting_options_dropdown_menu);
+        SharedPreferences sortingOptionSP = getSharedPreferences("SortingOption", MODE_PRIVATE);
+        String sortingOption = sortingOptionSP.getString("sortingOption", "Default");
+        String[] sortingOptions = { "Default", "Prioritize Recent", "Prioritize Small Classes" };
+
+        // Reinitialize the BoF list as sorted by the previous sorting option selected by the User
+        for (int i = 0; i < sortingOptions.length; i++) {
+            if (sortingOption.equals(sortingOptions[i])) { sortingOptionDropdown.setSelection(i); }
+        }
+    }
+
+    public void checkStateOfSearchButton() {
+        Log.d("HomePageActivity::checkStateOfSearchButton()", "Non-testable method");
+
+        // TODO: Instead of using Extras, use SharedPreferences instead to save the state of the
+        //  search button. This lessens the amount of code needed.
+        // TODO: Use SharedPreferences to save which sorting option was selected.
+        //Bundle extras = getIntent().getExtras();
 
         db = AppDatabase.singleton(getApplicationContext());
+        SharedPreferences searchButtonSP = getSharedPreferences("SearchButton", MODE_PRIVATE);
+        this.searchButtonState = searchButtonSP.getBoolean("searchButtonState", false);
+        TextView searchButton = findViewById(R.id.start_button);
 
-        // FIXME: testing
-        sortingOptionsDropdown = findViewById(R.id.sorting_options_dropdown_menu);
-        sortingOptionsDropdown.setOnItemSelectedListener(new SortingOptions());
-        sortingOptionsDropdown.setClickable(false);
-
-        Start = false;
-
-        Bundle extras = getIntent().getExtras();
-        if (extras!=null){
-            if(extras.containsKey("start")){
-                Start = extras.getBoolean("start",false);
-            };
+        if (this.searchButtonState) {
+            searchButton.setText(Constants.STOP);
+            compareUserCoursesWithStudents(AppDatabase.singleton(getApplicationContext()));
         }
+        else { searchButton.setText(Constants.START); }
 
-        TextView topLeftButton = findViewById(R.id.start_button);
-
-        if(Start){
-            topLeftButton.setText(Constants.STOP);
-        }
-        else{
-            topLeftButton.setText(Constants.START);
-        }
-
-        String currText = topLeftButton.getText().toString();
-        if (currText.equals(Constants.STOP)) {
-            compareUserCoursesWithStudents();
-        }
-
-
-        displayBirdsOfAFeatherList(new DefaultBoFComparator(db.BoFCourseDao()));
+        displayBirdsOfAFeatherList(db.BoFStudentDao(), new DefaultBoFComparator(db.BoFCourseDao()));
     }
 
     public void onStartClicked(View view) {
+        Log.d("HomePageActivity::onStartClicked()", "Non-testable method");
+
+        Spinner sortingOptionsDropdown = findViewById(R.id.sorting_options_dropdown_menu);
         TextView topLeftButton = findViewById(R.id.start_button);
         String currText = topLeftButton.getText().toString();
 
+        // If the User has ended the student search, then we maintain the list as is -- otherwise
+        // the list is displayed by default sorting
         if (currText.equals(Constants.START)) {
-            Start = true;
-
-            this.sortingOptionsDropdown.setClickable(true);
-
+            this.searchButtonState = true;
             topLeftButton.setText(Constants.STOP);
-            compareUserCoursesWithStudents();
+            sortingOptionsDropdown.setSelection(0);
+            compareUserCoursesWithStudents(AppDatabase.singleton(getApplicationContext()));
+            displayBirdsOfAFeatherList(db.BoFStudentDao(), new DefaultBoFComparator(db.BoFCourseDao()));
         }
         else {
+            this.searchButtonState = false;
             topLeftButton.setText(Constants.START);
-            Start = false;
-
-            this.sortingOptionsDropdown.setClickable(false);
         }
-
-        displayBirdsOfAFeatherList(new DefaultBoFComparator(db.BoFCourseDao()));
     }
 
     public void onBackClicked(View view) {
+        Log.d("HomePageActivity::onBackClicked()", "Non-testable method");
+
         Intent intent = new Intent(this, MainPageActivity.class);
+        //intent.putExtra("start", this.searchButtonState);
+        saveSortingOption();
+        saveSearchButtonState();
         startActivity(intent);
     }
 
     public void onMockNearbyMessagesClicked(View view) {
+        Log.d("HomePageActivity::onMockNearbyMessagesClicked()", "Non-testable method");
+
         Intent intent = new Intent(this, MockNearbyMessagesActivity.class);
-        intent.putExtra("start", Start);
+        //intent.putExtra("start", this.searchButtonState);
+        saveSortingOption();
+        saveSearchButtonState();
         startActivity(intent);
     }
 
@@ -145,7 +190,7 @@ public class HomePageActivity extends AppCompatActivity {
      * Adds the students and courses that the User has shared a class with to the BoF database by
      * cross-checking the User's courses with all the pre-populated courses in the database
      **/
-    public void compareUserCoursesWithStudents() {
+    public void compareUserCoursesWithStudents(AppDatabase db) {
         List<UserCourse> ucl = db.UserCourseDao().getAll();
         List<DefaultCourse> dcl;
         List<BoFStudent> bsl;
@@ -240,9 +285,8 @@ public class HomePageActivity extends AppCompatActivity {
         }
     }
 
-    public void displayBirdsOfAFeatherList(BoFComparator comparator) {
-        db = AppDatabase.singleton(getApplicationContext());
-        List<BoFStudent> students = db.BoFStudentDao().getAll();
+    public void displayBirdsOfAFeatherList(BoFStudentDao bsd, BoFComparator comparator) {
+        List<BoFStudent> students = bsd.getAll();
 
         studentsRecyclerView = findViewById(R.id.students_view);
         studentsLayoutManager = new LinearLayoutManager(this);
@@ -252,11 +296,13 @@ public class HomePageActivity extends AppCompatActivity {
         studentsRecyclerView.setAdapter(studentsViewAdapter);
     }
 
-    public void initSortingOptions() {
+    public void initSortingOptionsDropdown() {
+        Log.d("HomePageActivity::initSortingOptionsDropdown()", "Non-testable method");
         Spinner sortingOptionsDropdown = findViewById(R.id.sorting_options_dropdown_menu);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.sorting_options, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         sortingOptionsDropdown.setAdapter(adapter);
+        sortingOptionsDropdown.setOnItemSelectedListener(new SortingOptions());
     }
 }
